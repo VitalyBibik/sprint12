aa59edbb5745665b104a7f0217e6569f5efc9386
VitalyBibik <61142441+VitalyBibik@users.noreply.github.com>
2020-04-30 15:33:41 +0300

    Пушу 1 версию проекта

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { PORT = 3000 } = process.env;
+const app = express();
+const userRoutes = require('./routes/users');
+const cardRoutes = require('./routes/cards');
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.use('/', userRoutes);
+app.use('/', cardRoutes);
+
+
+app.get('*', (req, res) => {
+  res.status(404).send(	{ "message": "Запрашиваемый ресурс не найден" });
+});
+
+
+
+app.listen(PORT, () => {
+  console.log('Взлетаю без падения');
+});
---
+++ routes/cards.js
@@ -0,0 +1,12 @@
+const routes = require('express').Router();
+const cards = require('../data/cards');
+
+routes.get('/cards', (req, res) => {
+    res.status(200).json(cards);
+});
+
+module.exports = routes;
+
+
+
+
