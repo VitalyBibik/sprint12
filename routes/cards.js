4651028be396d42df7dd582e9e36ca0501a2b866
VitalyBibik <61142441+VitalyBibik@users.noreply.github.com>
2020-05-17 20:56:04 +0300

    Add new featch in routes/cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,10 @@
 const routes = require('express').Router();
-const { getCards, deleteCard, createCard } = require('../controllers/cards');
+const { getCards, deleteCard, createCard, likeCard, dislikeCard } = require('../controllers/cards');
 
 routes.get('/cards', getCards);
 routes.post('/cards', createCard);
 routes.delete('/cards/:cardId', deleteCard);
-
+routes.put('/cards/:cardId/likes', likeCard);
+routes.delete('/cards/:cardId/likes', dislikeCard);
 
 module.exports = routes;
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const { getUsers, createUser, getUser } = require('../controllers/users');
 
 routes.get('/users', getUsers);
 routes.post('/users', createUser);
-
 routes.all('/users/:id', getUser);
+
 
 module.exports = routes;
