f36fb8cbab0dd49fc82584422b87122a7073a3c4
VitaliyBibik <Wital2010@gmail.com>
2020-06-19 20:43:22 +0300

    Change Promise.reject on Throw

--- controllers/cards.js
+++ controllers/cards.js
@@ -18,7 +18,7 @@ module.exports.deleteCard = async (req, res, next) => {
     const userDeleteCard = await Card.findById(cardId).populate('owner')
       .orFail(() => new NotFoundError('The card was already deleted Or Cardlist is empty'));
     if (!userDeleteCard.owner.equals(req.user._id)) {
-      await Promise.reject(new AccessDeniedError('Access denied'));
+      throw new AccessDeniedError('Access denied');
     }
     await userDeleteCard.remove();
     return res.send(userDeleteCard);
